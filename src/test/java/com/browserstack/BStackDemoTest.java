7c53559e9750f8024c3fd36219348007a2ec4455
harshbrowserstack <119026745+harshbrowserstack@users.noreply.github.com>
2023-03-02 20:52:11 +0530

    Revert "third commit"
    This reverts commit ec3d7f4a3d8c9c4c7e7fb9a9e871205a901cf613.

--- src/test/java/com/browserstack/BStackDemoTest.java
+++ src/test/java/com/browserstack/BStackDemoTest.java
@@ -54,8 +54,7 @@ public class BStackDemoTest extends SeleniumTest {
         driver.get("https://www.bstackdemo.com");
 
         // Check the title
-        // Assert.assertTrue(driver.getTitle().matches("StackDemo")); // This will Pass
-        Assert.assertTrue(driver.getTitle().matches("Stack")); //This will Fail
+        Assert.assertTrue(driver.getTitle().matches("StackDemo"));
 
         // Save the text of the product for later verify
         String productOnScreenText = driver.findElement(By.xpath("//*[@id=\"1\"]/p")).getText();
@@ -81,9 +80,8 @@ public class BStackDemoTest extends SeleniumTest {
         // Save the text of the product for later verify
         String productOnScreenText = driver.findElement(By.xpath("//*[@id=\"1\"]/p")).getText();
         // Click on add to cart button
-        // driver.findElement(By.xpath("//*[@id=\"1\"]/div[4]")).click(); //This will pass
-        driver.findElement(By.xpath("//*[@id=\"1\"]/div[]")).click(); //This will fail
-        
+        driver.findElement(By.xpath("//*[@id=\"1\"]/div[4]")).click();
+
         // See if the cart is opened or not
         Assert.assertTrue(driver.findElement(By.cssSelector(".float\\-cart__content")).isDisplayed());
 
@@ -92,7 +90,7 @@ public class BStackDemoTest extends SeleniumTest {
         Assert.assertEquals(productOnScreenText, productOnCartText);
     }
     
-    @Test(enabled=false)
+    @Test
     public void removeProductToCart() throws Exception {
         // navigate to bstackdemo
         driver.get("https://www.bstackdemo.com");
@@ -133,6 +131,7 @@ public class BStackDemoTest extends SeleniumTest {
         String productOnCartText = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).getText();
         Assert.assertEquals(productOnScreenText, productOnCartText);
     }
+    
     
     @Test
     public void placeOrder() throws Exception {
